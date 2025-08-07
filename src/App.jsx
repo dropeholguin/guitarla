33699edee713a78438c1b1d0b04d108f84ec50c7
pedroholguin <dropeholguin1994@gmail.com>
2025-08-07 18:22:23 -0500

    New components and logic are added to add to the shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import { Header } from "./components/Header";
+import { db } from "./data/db";
+
 function App() {
+  const [data] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((cartItem) => cartItem.id === item.id);
+    if (itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      setCart([...cart, { ...item, quantity: 1 }]);
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart((prevCart => prevCart.filter(guitar => guitar.id !== id)));
+  }
+
   return (
     <>
-     <h1>GUITAR LA</h1>
+      <Header cart={cart} removeFromCart={removeFromCart} />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
